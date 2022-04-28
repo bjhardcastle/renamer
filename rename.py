b4ef9bf21d50d27fd2a0c75a36ce00110daba640
bjhardcastle <ben.hardcastle@alleninstitute.org>
2022-04-27 19:03:53 -0700

    add dialog to confirm overwrite or quit, move arg parsing and test

--- rename.py
+++ rename.py
@@ -8,43 +8,58 @@ import tkinter as tk
 from pathlib import Path
 from tkinter.filedialog import askdirectory
 
+# define some globals here for visibility:
 
-def main(args=[]):
+# starting dir in case folder select gui is opened - need not exist
+initial_gui_dir = r"\\W10DTSM112719\neuropixels_data"
 
-    if args:
-        # parse input arguments
-        parser = argparse.ArgumentParser(
-            description="Find and replace in the names and content of files and their folders non-destructively: new folder is created with renamed contents, original folder is preserved. Intended for changing mouseID or sessionID in \\neuropixels_data. "
-        )
-        parser.add_argument("-folder", type=str, help="path to folder with files for renaming")
-        parser.add_argument(
-            "-old_string", type=str, help="replace all instances of this string in paths and within file contents"
-        )
-        parser.add_argument("-new_string", type=str, help="replace instances of old-string with this string ")
-        args = parser.parse_args(args)
-        rename_folder, old_str, new_str = args.folder, args.old_string, args.new_string
-        # globals().update(vars(args))
+# path substrings to exclude from contents renaming - need not be exhaustive, can just save time instead of trying to open large non-text file
+skip_content_edit_list = [".mp4", ".png", "motor-locs.csv", ".pkl"]
 
-    # if args == []:
-    else:
-        # open folder selection
-        root = tk.Tk()  # don't call more than one instance! use Toplevel for each window
-        root.withdraw()
-        win1 = tk.Toplevel()
-        win1.withdraw()
-        rename_folder = askdirectory(initialdir=r"\\W10DTSM112719\neuropixels_data", title="Choose folder to rename",)
-        win1.quit()
+
+def parse_args():
+    # parse input arguments and add helpful info
+    parser = argparse.ArgumentParser(
+        description="Find and replace in the names and content of files and their folders non-destructively: new folder is created with renamed contents, original folder is preserved. Intended for changing mouseID or sessionID in \\neuropixels_data. "
+    )
+    parser.add_argument("-folder", type=str, help="path to folder with files for renaming")
+    parser.add_argument(
+        "-old_string", type=str, help="replace all instances of this string in paths and within file contents"
+    )
+    parser.add_argument("-new_string", type=str, help="replace instances of old-string with this string ")
+    args = parser.parse_args()
+
+    # globals().update(vars(args))
+    return args.folder, args.old_string, args.new_string
+
+
+def main(rename_folder=None, old_str=None, new_str=None):
+
+    # open folder selection
+    root = tk.Tk()  # don't call more than one instance! use Toplevel for each window
+    root.withdraw()
+
+    if not rename_folder:
+
+        # win1 = tk.Toplevel()
+        # win1.withdraw()
+        rename_folder = askdirectory(initialdir=initial_gui_dir, title="Choose folder to rename",)
+        # win1.quit()
+        # root.destroy()
         if not rename_folder:
             print("Folder selection cancelled: quitting...")
             quit()
 
-        # suggest session number / mouse id substrings as a starting point for renaming
-        try_str = re.search("[0-9]{10}_[0-9]{6}_[0-9]{8}", rename_folder)
-        try_str = try_str[0] if try_str is not None else ""
+    # suggest session number / mouse id substrings as a starting point for renaming
+    try_str = re.search("[0-9]{10}_[0-9]{6}_[0-9]{8}", rename_folder)
+    try_str = try_str[0] if try_str is not None else ""
 
+    if not old_str or not new_str:
         old_str, new_str = replace_substr_dialog(try_str)
 
     destination_path = rename_folder.replace(old_str, new_str)
+    if Path(destination_path).exists():
+        check_before_overwrite_dialog(destination_path)  # quits if exists and user decides not to overwrite
     new_dir = rename_by_copy(old_str, new_str, rename_folder, recursive=True, copy_nontext_files=True)
 
     # if any parts of newstring and oldstring are the same, extract out the substring(s) which changed and run again to replace isolated instanced
@@ -54,10 +69,14 @@ def main(args=[]):
     for idx, substr in enumerate(new_substr):
         if substr != old_substr[idx]:
             _ = rename_by_copy(old_substr[idx], substr, new_dir, recursive=True, copy_nontext_files=True)
+    # TODO delete partially-renamed directories in destination folder after this last step: can't just compare subfolders in rename_folder and new_dir and delete exact matches, since they may be subfolders which weren't renamed at all
 
     # open new folder
+    print(
+        f"\nNew, renamed files are at {destination_path}. \nOriginal folder should be deleted manually after checking."
+    )
+    os.startfile(rename_folder)
     os.startfile(destination_path)
-    print(f"Finished. New, renamed files are at {destination_path}. \nOriginal folder can be deleted manually.")
 
 
 def rename_by_copy(old_str, new_str, old_dir, recursive=True, copy_nontext_files=True) -> str:
@@ -67,14 +86,13 @@ def rename_by_copy(old_str, new_str, old_dir, recursive=True, copy_nontext_files
     rename_paths = Path(old_dir).glob(strexp)
     # for old_path in rename_paths:
     for old_path in progressbar(list(rename_paths), "Copying to new folder(s): ", 40):
-        time.sleep(0.1)  # any code you need
 
         new_path = str(old_path).replace(old_str, new_str)
         if Path(new_path).suffix:
             Path(new_path).parents[0].mkdir(parents=True, exist_ok=True)
             try:
-                skip = False
-                if not any([sub in new_path for sub in [".mp4", ".png", "motor-locs.csv", ".pkl"]]):
+                already_copied = False
+                if not any([sub in new_path for sub in skip_content_edit_list]):
                     # skip some large files
                     with open(old_path, "rt") as file:
                         x = file.read()
@@ -82,8 +100,10 @@ def rename_by_copy(old_str, new_str, old_dir, recursive=True, copy_nontext_files
                         x = x.replace(old_str, new_str)
                         file.write(x)
                         skip = True
+            except UnicodeDecodeError:
+                continue
             finally:
-                if copy_nontext_files and not skip:
+                if copy_nontext_files and not already_copied:
                     try:
                         shutil.copy2(old_path, new_path)  # copy with created/modified times preserved
                     except shutil.SameFileError:
@@ -97,14 +117,33 @@ def rename_by_copy(old_str, new_str, old_dir, recursive=True, copy_nontext_files
     return old_dir.replace(old_str, new_str)
 
 
+def check_before_overwrite_dialog(path):
+    import tkinter as tk
+
+    def b1_callback():
+        quit()  # exit program
+
+    def b2_callback():
+        win3.quit()  # exit gui and continue
+
+    win3 = tk.Toplevel()
+    label = tk.Label(win3, text="Contents may be overwritten:").grid(row=0, column=0)
+    button_1 = tk.Button(win3, text="Cancel", command=b1_callback).grid(row=1, column=0)
+    button_2 = tk.Button(win3, text="Continue", command=b2_callback).grid(row=1, column=1)
+    # button_2.pack()
+    win3.title("Folder exists")
+    win3.mainloop()
+    return None
+
+
 def replace_substr_dialog(suggest_old="", suggest_new="") -> (str, str):
     """ open dialog box, ask user old string to find in filepaths and replace with new string """
 
     if suggest_old + suggest_new == suggest_old:  # old str suggested, but not new
         suggest_new = "".join(suggest_old)  # ...so repeat old str in new box for easier editing
 
-    root = tk.Tk()  # don't call more than one instance! use Toplevel for each window
-    root.withdraw()
+    # root = tk.Tk()  # don't call more than one instance! use Toplevel for each window
+    # root.withdraw()
     win2 = tk.Toplevel()
     old_str = tk.StringVar()
     old_str.set(suggest_old)
@@ -118,7 +157,8 @@ def replace_substr_dialog(suggest_old="", suggest_new="") -> (str, str):
     win2.bind("<Return>", lambda event: win2.quit())
     win2.title("Enter substring in path to replace")
     win2.mainloop()
-    return old_str.get(), new_str.get()
+    old_str, new_str = old_str.get(), new_str.get()
+    return old_str, new_str
 
 
 def progressbar(it, prefix="", size=60, file=sys.stdout):
@@ -139,4 +179,7 @@ def progressbar(it, prefix="", size=60, file=sys.stdout):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    args = parse_args()
+    main(*args)
+    sys.exit()
+
